5231eb7bdbec5e2d205b226f9bc7a2e6f635d1cb
geritol <gergo.tolnai@gmail.com>
2017-10-23 17:10:18 +0200

    refactor

--- game.js
+++ game.js
@@ -1,3 +1,58 @@
+function generateNewBoard(rows, columns, mineCountToGenerate){
+    const cellCount = rows * columns
+    if(mineCountToGenerate > cellCount){
+      throw new Error('More mines specified than cells!')
+    }
+    let board = range(rows).map(function(){return range(columns).map(function(){return {}})})
+    let mineCoordinates = []
+    let mineCoordinatesIndex = {}
+    while(mineCoordinates.length < mineCountToGenerate){
+        let x = Math.floor(Math.random() * columns)
+        let y = Math.floor(Math.random() * rows)
+        if(!mineCoordinatesIndex[String([x,y])]){
+            mineCoordinatesIndex[String([x,y])] = 1
+            mineCoordinates.push([x,y])
+            board[y][x].mine = true
+        }
+    }
+    board.forEach(function(row, rowIndex){
+        row.map(function(cell, columnsIndex){
+            cell = calculateNearbyMines(rowIndex, columnsIndex, board)
+        })
+    })
+    return board
+}
+
+function calculateNearbyMines(rowIndex, columnIndex, board){
+    let cell = board[rowIndex][columnIndex]
+    if(cell.mine) return cell
+
+    let nearbyMinesCount = 0
+
+    getNearbyCells(rowIndex, columnIndex, board).forEach(function(cell){
+        if(cell.mine) nearbyMinesCount += 1
+    })
+
+    cell.nearbyMines = nearbyMinesCount
+    return cell
+}
+
+function handleClick(event){
+  let [x,y] = pixelToCoordinates(event.offsetX, event.offsetY, config)
+  board = move(x,y, board)
+  render(board, config)
+}
+
+function handleRightClick(event){
+  event.preventDefault()
+  let [x,y] = pixelToCoordinates(event.offsetX, event.offsetY, config)
+  let cell = board[y][x]
+  if(!cell.shouldShow && !cell.reveal){
+      cell.flag = !cell.flag
+  }
+  render(board, config)
+}
+
 function move(x,y, board){
     let cell = board[y][x]
 
---
+++ helper.js
@@ -0,0 +1,30 @@
+function getNearbyCells(rowIndex, columnIndex, board){
+    let yDeltas = range(-1,2)
+    let xDeltas = range(-1,2)
+    let nearbyCells = []
+
+    yDeltas.forEach(function(yDelta){
+        xDeltas.forEach(function(xDelta){
+            // if we have the current cell, we don't want to go further
+            if(yDelta === 0 && xDelta === 0) return
+            let nearbyY = rowIndex + yDelta
+            let nearbyX = columnIndex + xDelta
+            if(nearbyX < 0 || nearbyY < 0) return
+            if(nearbyY + 1 > board.length || nearbyX + 1 > board[0].length) return
+            let nearbyCell = board[nearbyY][nearbyX]
+            nearbyCell.xCoordinate = nearbyX
+            nearbyCell.yCoordinate = nearbyY
+            nearbyCells.push(nearbyCell)
+        })
+    })
+
+    return nearbyCells
+}
+
+function range(lower, upper){
+    if(!upper){
+        upper = lower
+        lower = 0
+    }
+    return Array.apply(null, Array(upper + Math.abs(lower))).map(function (_, j) {return j + lower;});
+}
