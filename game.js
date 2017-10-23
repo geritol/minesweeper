08b52b7630c64e269bddac7695833fc7f207a684
geritol <gergo.tolnai@gmail.com>
2017-10-23 20:50:42 +0200

    moved out getNearbyCells from helper

--- game.js
+++ game.js
@@ -95,6 +95,34 @@ function calculateNearbyMines(rowIndex, columnIndex, board){
     return cell
 }
 
+function getNearbyCells(rowIndex, columnIndex, board){
+  /*
+    IN: rowIndex (integer), columnIndex (integer), board (2d array)
+    OUT: list of cells
+    DESC: return cells that share an edge or a corner with the input cell
+  */
+    let yDeltas = range(-1,2)
+    let xDeltas = range(-1,2)
+    let nearbyCells = []
+
+    yDeltas.forEach(function(yDelta){
+        xDeltas.forEach(function(xDelta){
+            // if we have the current cell, we don't want to go further
+            if(yDelta === 0 && xDelta === 0) return
+            let nearbyY = rowIndex + yDelta
+            let nearbyX = columnIndex + xDelta
+            if(nearbyX < 0 || nearbyY < 0) return
+            if(nearbyY + 1 > board.length || nearbyX + 1 > board[0].length) return
+            let nearbyCell = JSON.parse(JSON.stringify(board[nearbyY][nearbyX]))
+            nearbyCell.xCoordinate = nearbyX
+            nearbyCell.yCoordinate = nearbyY
+            nearbyCells.push(nearbyCell)
+        })
+    })
+
+    return nearbyCells
+}
+
 function handleClick(event){
   /*
     IN: event left click
--- helper.js
+++ helper.js
@@ -1,27 +1,3 @@
-function getNearbyCells(rowIndex, columnIndex, board){
-    let yDeltas = range(-1,2)
-    let xDeltas = range(-1,2)
-    let nearbyCells = []
-
-    yDeltas.forEach(function(yDelta){
-        xDeltas.forEach(function(xDelta){
-            // if we have the current cell, we don't want to go further
-            if(yDelta === 0 && xDelta === 0) return
-            let nearbyY = rowIndex + yDelta
-            let nearbyX = columnIndex + xDelta
-            if(nearbyX < 0 || nearbyY < 0) return
-            if(nearbyY + 1 > board.length || nearbyX + 1 > board[0].length) return
-            let nearbyCell = JSON.parse(JSON.stringify(board[nearbyY][nearbyX]))
-            nearbyCell.xCoordinate = nearbyX
-            nearbyCell.yCoordinate = nearbyY
-            nearbyCells.push(nearbyCell)
-        })
-    })
-
-    return nearbyCells
-}
-
-
 function coordinatesToPixel(x,y, {cellSize, separatorLineThickness}){
     let xPx = cellSize * x + separatorLineThickness * (x + 1)
     let yPx = cellSize * y + separatorLineThickness * (y + 1)
