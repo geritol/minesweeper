e718d3d227cd574b85222e065b90526f42c23518
Mate Bodor <bodormate@gmail.com>
2017-10-23 19:15:36 +0200

    add description to game functions

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+'use strict'
 /*
   Board: 2d Array containing cells
   Example:
@@ -17,11 +18,11 @@
  */
 
 function generateNewBoard(rows, columns, mineCountToGenerate){
-    /*
-      rowCount (integer), columnCount (integer), mineCountToGenerate (integer)
-      -> board (2d array), contains cells (object) {mine: Boolean, Integer}
-      Crerates a board, fills it with mines, and calculates nearby mine counts for cells
-    */
+  /*
+    IN: rowCount (integer), columnCount (integer), mineCountToGenerate (integer)
+    OUT: board (2d array), contains cells (object) {mine: Boolean, Integer}
+    DESC: Crerates a board, fills it with mines, and calculates nearby mine counts for cells
+  */
 
     // create empty board
     let board = range(rows).map(function(){return range(columns).map(function(){return {}})})
@@ -46,12 +47,21 @@ function generateNewBoard(rows, columns, mineCountToGenerate){
 }
 
 function generateMineCoordinates(columns, rows, mineCountToGenerate){
+  /*
+    IN: rowCount (integer), columnCount (integer), mineCountToGenerate (integer)
+    OUT: mineCoordinates (array), contains coordinate of mines: [[3,4],[4,6]]
+    DESC: Crerates an array, that contains the coordinates of all the mines. All mine have to be unique coordinate.
+  */
   const cellCount = rows * columns
   if(mineCountToGenerate > cellCount){
     throw new Error('More mines specified than cells!')
   }
   let mineCoordinates = []
   let mineCoordinatesIndex = {}
+
+  // generate random coordinates until reach the necessary number of mines.
+
+  // check if the new generated coordinate is unique
   while(mineCoordinates.length < mineCountToGenerate){
       let x = Math.floor(Math.random() * columns)
       let y = Math.floor(Math.random() * rows)
@@ -64,11 +74,20 @@ function generateMineCoordinates(columns, rows, mineCountToGenerate){
 }
 
 function calculateNearbyMines(rowIndex, columnIndex, board){
+  /*
+    IN: rowCount (integer), columnCount (integer), board (2d array)
+    OUT: cell (object) posiible value {"nearbyMines":0} or {"mine":true}
+    DESC: Crerates an object, that contains the information regarding one cell
+  */
+
+  //Check if cell is not mine
     const cell = board[rowIndex][columnIndex]
     if(cell.mine) return cell
 
     let nearbyMinesCount = 0
 
+
+    //Use getNearbyCells helper function to get all the nearby cell
     getNearbyCells(rowIndex, columnIndex, board).forEach(function(cell){
         if(cell.mine) nearbyMinesCount += 1
     })
@@ -78,12 +97,27 @@ function calculateNearbyMines(rowIndex, columnIndex, board){
 }
 
 function handleClick(event){
+  /*
+    IN: event left click
+    OUT:
+    DESC: based on mouse click recalculate table and render it
+  */
+
+  // use pixelToCoordinates helper to get the coordiante
   let [x,y] = pixelToCoordinates(event.offsetX, event.offsetY, config)
+
+  //use move to go throught the board
   board = move(x,y, board)
+  //render the board
   render(board, config)
 }
 
 function handleRightClick(event){
+  /*
+    IN: event right click
+    OUT:
+    DESC: based on mouse click recalculate table and render it and cell.flag
+  */
   event.preventDefault()
   let [x,y] = pixelToCoordinates(event.offsetX, event.offsetY, config)
   let cell = board[y][x]
@@ -94,6 +128,11 @@ function handleRightClick(event){
 }
 
 function move(x,y, board){
+  /*
+    IN: x (integer), y (integer), board (2d array)
+    OUT: board (2d array), contains cells (object) {mine: Boolean, Integer}
+    DESC: recalculate board and show results of the event
+  */
     let cell = board[y][x]
 
     // no ations are needed
@@ -106,7 +145,7 @@ function move(x,y, board){
       return board
     }
 
-    // show nearby
+    // show nearby use showNearbyCells
     if(!cell.nearbyMines) showNearbyCells(x,y,board)
 
     // show this cell only
@@ -115,6 +154,13 @@ function move(x,y, board){
 }
 
 function showNearbyCells(x,y, board){
+  /*
+    IN: x (integer), y (integer), board (2d array)
+    OUT:
+    DESC: recrusivley check nearby cells and show them if empty
+  */
+
+  //use getNearbyCells from helper
     getNearbyCells(y,x,board).forEach(function(nearbyCell) {
         let boardCell = board[nearbyCell.yCoordinate][nearbyCell.xCoordinate]
         if(!nearbyCell.nearbyMines && !boardCell.shouldShow){
@@ -126,6 +172,11 @@ function showNearbyCells(x,y, board){
 }
 
 function revealAll(board){
+  /*
+    IN:  board (2d array)
+    OUT:
+    DESC: rset reveal all cell
+  */
     board.forEach(function(row){
         row.forEach(function(cell){
             cell.reveal = true
